c6e66a64e647a742ffd6439d1b82348a9402b5c4
auelbai <auelbai12@mail.ru>
2022-07-03 17:28:27 +0600

    add DI in WorkerFactory

--- app/src/main/java/com/example/cryptoapp/data/workers/CoinWorker.kt
+++ app/src/main/java/com/example/cryptoapp/data/workers/CoinWorker.kt
@@ -6,19 +6,20 @@ import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkerParameters
 import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.database.CoinInfoDao
 import com.example.cryptoapp.data.mapper.CoinMapper
 import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.network.ApiService
 import kotlinx.coroutines.delay
 import java.lang.Exception
 
 class CoinWorker(
     context: Context,
-    workerParameters: WorkerParameters
+    workerParameters: WorkerParameters,
+    private val apiService: ApiService,
+    private val coinInfoDao: CoinInfoDao,
+    private val mapper: CoinMapper
 ) : CoroutineWorker(context, workerParameters) {
-
-    private val apiService = ApiFactory.apiService
-    private val coinInfoDao = AppDatabase.getInstance(context).coinInfoDao()
-    private val mapper = CoinMapper()
 
     override suspend fun doWork(): Result {
         while (true) {
@@ -40,7 +41,7 @@ class CoinWorker(
     companion object {
         const val NAME = "CoinWorker"
 
-        fun makeRequest() : OneTimeWorkRequest {
+        fun makeRequest(): OneTimeWorkRequest {
             return OneTimeWorkRequestBuilder<CoinWorker>().build()
         }
     }
--- app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
@@ -1,17 +1,20 @@
 package com.example.cryptoapp.di
 
 import android.app.Application
+import com.example.cryptoapp.CryptoApp
 import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.example.cryptoapp.presentation.CoinPriceListActivity
 import com.example.cryptoapp.presentation.ViewModelModule
 import dagger.BindsInstance
 import dagger.Component
 
+@ApplicationScope
 @Component(modules = [DataModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
     fun inject(activity: CoinPriceListActivity)
     fun inject(fragment: CoinDetailFragment)
+    fun inject(application: CryptoApp)
 
     @Component.Factory
     interface Factory {
